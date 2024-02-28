8a3993c69331fc33eaf83f385d7f6411ef6ec348
Simon Dalvai <s.dalvai@noi.bz.it>
2024-02-28 13:44:08 +0100

    fix reuse

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 window.onload = function () {
     // locations where footer will be hidden
     const locations = ['foyer', 'd1', 'd6']
